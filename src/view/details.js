c21c18116339167bc15d8c6a954ae9fa76772b57
lanshayu <fingnet@gmail.com>
2015-07-22 13:31:38 +0800

    Details Update

--- src/view/details.js
+++ src/view/details.js
@@ -8,7 +8,7 @@ define(function(require){
 		render:function(){
 			this.$el.append('<tbody></tbody>');
 			_.each(this.schema, function(item){
-				this.$('tbody').append('<tr><th width="160" style="background-color: #f9f9f9;">' + item.alias + '</th><td style="background-color: #ffffff;">' + this.model.get(item.name)+'</td></tr>');
+				this.$('tbody').append('<tr><th width="160" style="background-color: #f9f9f9;">' + item.alias + '</th><td style="background-color: #ffffff;">' + (this.model.get(item.name) ? this.model.get(item.name) : '') +'</td></tr>');
 			}, this);
 			return this;
 		}
