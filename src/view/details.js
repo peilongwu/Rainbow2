410b5998fed4565b47f21be2afbf9f06bb28d712
lanshayu <fingnet@gmail.com>
2015-07-22 14:27:36 +0800

    Details Update

--- src/view/details.js
+++ src/view/details.js
@@ -8,7 +8,14 @@ define(function(require){
 		render:function(){
 			this.$el.append('<tbody></tbody>');
 			_.each(this.schema, function(item){
-				this.$('tbody').append('<tr><th width="160" style="background-color: #f9f9f9;">' + item.alias + '</th><td style="background-color: #ffffff;">' + (this.model.get(item.name) ? this.model.get(item.name) : '') +'</td></tr>');
+				var title = item.alias ? item.alias : item.name;
+				var value = this.model.get(item.name) ? this.model.get(item.name) : '';
+				this.$('tbody')
+				.append(
+					'<tr><th width="160" style="background-color: #f9f9f9;">' 
+					+ title + '</th><td style="background-color: #ffffff;">'
+					+ value +'</td></tr>'
+				);
 			}, this);
 			return this;
 		}
